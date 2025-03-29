9f61be2248114fb7cd28b64e5f8639ae045eeff5
Mateusz Martyna <mateuszmartyna44@gmail.com>
2025-03-29 09:03:38 +0100

    init: Initialized shadcn as a UI library

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen">
       <h1>Hello World</h1>
+      <Button >
+        Test
+      </Button>
     </div>
   );
 }
